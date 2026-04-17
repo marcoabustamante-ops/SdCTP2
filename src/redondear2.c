844ee47ec70481d683de5b1254d7860900ad217d
Luciano Blanco <luciano.blanco@mi.unc.edu.ar>
2026-04-17 07:43:56 -0300

    Corrección redondear2.c y operacion_asm.s

--- src/redondear2.c
+++ src/redondear2.c
@@ -1,8 +1,5 @@
-// Declaramos la función externa que escribiste en Ensamblador
 extern int operacion_asm(float valor);
 
-// La función que será invocada desde Python vía ctypes
 int redondear(float valor) {
-    // C actúa como puente y le pasa el float a la capa de ASM
     return operacion_asm(valor); 
 }
